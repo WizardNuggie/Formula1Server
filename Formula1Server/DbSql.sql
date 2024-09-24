c56e8f7a674c54c540dd6bf2fce15b2d9e3a36cc
User <User@LAB3-12>
2024-09-24 13:00:39 +0300

    Add project files.

---
+++ Formula1Server/DbSql.sql
@@ -0,0 +1,35 @@
+﻿Use master
+Go
+IF EXISTS (SELECT * FROM sys.databases WHERE name = N'Dbsql')
+BEGIN
+    DROP DATABASE Dbsql;
+END
+Go
+Create Database Dbsql
+Go
+Use Dbsql
+Go
+
+-- creation of the user types table --
+CREATE TABLE UserTypes (
+	[UserTypeId] INT PRIMARY KEY, -- PK
+	[UserTypeName] NVARCHAR (250) -- Name of the user type
+)
+
+-- creation of the users table --
+CREATE TABLE Users (
+	[Email] NVARCHAR (250) PRIMARY KEY, -- PK, user email
+	[UserId] INT IDENTITY(1,1), -- user ID
+	[Username] NVARCHAR(20), -- username
+	[Name] NVARCHAR (250), -- name of the user
+	[Password] NVARCHAR (20), -- password
+	[IsAdmin] BOOL, -- defines whether the user is an admin or not
+	[UserTypeId] INT, -- the id of the user type, FK from UserTypes table
+	FOREIGN KEY (UserTypeId) REFERENCES UserTypes(UserTypeId)
+)
+
+-- creation of the aricles table --
+CREATE TABLE Articles (
+	[ArticleId] INT IDENTITY (1,1) PRIMARY KEY, -- PK
+	
+)
