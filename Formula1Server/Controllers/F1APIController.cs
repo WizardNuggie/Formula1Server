1ea7eb7ebf2c39bacef8eacf92fbc3fbd224896c
Ro <talkazyo@gmail.com>
2025-06-09 22:35:37 +0300

    i think backup works

--- Formula1Server/Controllers/F1APIController.cs
+++ Formula1Server/Controllers/F1APIController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Formula1Server.Models;
 using Formula1Server.DTO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 
 namespace Formula1Server.Controllers
 {
@@ -658,6 +659,109 @@ namespace Formula1Server.Controllers
             {
                 return BadRequest(ex.Message);
             }
+        }
+        #endregion
+
+        #region Backup / Restore
+        [HttpGet("Backup")]
+        public async Task<IActionResult> Backup()
+        {
+            string path = $"{this.webHostEnvironment.WebRootPath}\\..\\DbScripts\\backup.bak";
+
+            bool success = await BackupDatabaseAsync(path);
+            if (success)
+            {
+                return Ok("Backup was successful");
+            }
+            else
+            {
+                return BadRequest("Backup failed");
+            }
+        }
+
+        [HttpGet("Restore")]
+        public async Task<IActionResult> Restore()
+        {
+            string path = $"{this.webHostEnvironment.WebRootPath}\\..\\DbScripts\\backup.bak";
+
+            bool success = await RestoreDatabaseAsync(path);
+            if (success)
+            {
+                return Ok("Restore was successful");
+            }
+            else
+            {
+                return BadRequest("Restore failed");
+            }
+        }
+        //this function backup the database to a specified path
+        private async Task<bool> BackupDatabaseAsync(string path)
+        {
+            try
+            {
+
+                //Get the connection string
+                string? connectionString = context.Database.GetConnectionString();
+                //Get the database name
+                string databaseName = context.Database.GetDbConnection().Database;
+                //Build the backup command
+                string command = $"BACKUP DATABASE {databaseName} TO DISK = '{path}'";
+                //Create a connection to the database
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    //Open the connection
+                    await connection.OpenAsync();
+                    //Create a command
+                    using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                    {
+                        //Execute the command
+                        await sqlCommand.ExecuteNonQueryAsync();
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        //THis function restore the database from a backup in a certain path
+        private async Task<bool> RestoreDatabaseAsync(string path)
+        {
+            try
+            {
+                //Get the connection string
+                string? connectionString = context.Database.GetConnectionString();
+                //Get the database name
+                string databaseName = context.Database.GetDbConnection().Database;
+                //Build the restore command
+                string command = $@"
+                USE master;
+                ALTER DATABASE {databaseName} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+                RESTORE DATABASE {databaseName} FROM DISK = '{path}' WITH REPLACE;
+                ALTER DATABASE {databaseName} SET MULTI_USER;";
+
+                //Create a connection to the database
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    //Open the connection
+                    await connection.OpenAsync();
+                    //Create a command
+                    using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                    {
+                        //Execute the command
+                        await sqlCommand.ExecuteNonQueryAsync();
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
         }
         #endregion
     }
---
+++ Formula1Server/DbScripts/BackupRestore.sql
@@ -0,0 +1,30 @@
+﻿Use master
+Go
+
+-- Create a login for the admin user
+CREATE LOGIN  [AdminLogin] WITH PASSWORD = 'rokazyo123';
+Go
+
+-- Create a user in the DB database for the login
+CREATE USER [AdminLogin] FOR LOGIN [AdminLogin];
+Go
+
+--so user can restore the DB!
+ALTER SERVER ROLE sysadmin ADD MEMBER [AdminLogin];
+Go
+
+Create Database AppServer_DB
+go
+
+
+
+Use master
+Go
+
+
+USE master;
+ALTER DATABASE AppServer_DB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+RESTORE DATABASE AppServer_DB FROM DISK = 'C:\Users\user\source\repos\WizardNuggie\Formula1Server\Formula1Server\wwwroot\..\DbScripts\backup.bak' WITH REPLACE, --להחליף את זה לנתיב של קובץ הגיבוי
+    MOVE 'AppServer_DB' TO 'C:\Users\user\AppServer_DB.mdf',   --להחליף לנתיב שנמצא על המחשב שלך
+    MOVE 'AppServer_DB_log' TO 'C:\Users\user\AppServer_DB_log.ldf';  
+ALTER DATABASE AppServer_DB SET MULTI_USER;
